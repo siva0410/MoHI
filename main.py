8526ffb853f7ce416c9bd975ea3eb37ce627f6a4
Siva0410 <nmkfngt@s.okayama-u.ac.jp>
2019-11-20 19:03:54 +0900

    modified

--- main.py
+++ main.py
@@ -46,9 +46,7 @@ def callback():
 def handle_message(event):
 	line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='「' + event.message.text +
-                        event.message.text + event.message.text + '」三倍返しだ
-        あ!')
+        TextSendMessage(text='「' + event.message.text + event.message.text + event.message.text + '」三倍返しだあ!')
      )
 
 if __name__ == "__main__":
