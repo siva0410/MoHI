0c66875a404d881f93e406dafa771e5a77b95ea9
Siva0410 <nmkfngt@s.okayama-u.ac.jp>
2019-11-20 18:52:14 +0900

    nani->dato

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def callback():
 def handle_message(event):
 	line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='「' + event.message.text + '」って何？')
+        TextSendMessage(text='「' + event.message.text + '」だと？!')
      )
 
 if __name__ == "__main__":
