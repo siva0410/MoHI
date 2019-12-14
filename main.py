d7e4f14c6e879f0e62023294a113c842be72ff88
Siva0410 <nmkfngt@s.okayama-u.ac.jp>
2019-12-15 06:16:30 +0900

    use timestamp

--- main.py
+++ main.py
@@ -106,8 +106,7 @@ def get_response_message(mes_from,usr_id):
 
     # "おはよう"が入力された時
     if "おはよ" in mes_from and flag_num == 2:
-        mes_from
-        hel_time = mes_from.timestamp
+        hel_time = datetime.fromtimestamp(event.timestamp/1000.0)
         mes = "おはようございます！\n 現在の時刻は{}時{}分{}秒です！"
         mes = mes.format(hel_time.hour,hel_time.minute,hel_time.second)
 
