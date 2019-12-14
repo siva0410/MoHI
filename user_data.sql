b81444792b3a3bbac0a69efd8ca93d57c1abf8aa
Siva0410 <nmkfngt@s.okayama-u.ac.jp>
2019-12-15 01:18:08 +0900

    db test

--- user_data.sql
+++ user_data.sql
@@ -1,11 +1,10 @@
-CREATE TABLE usr_data
+CREATE TABLE usr_data2
 (
-usr_id	TEXT	NOT NULL,
-usr_name	TEXT	NOT NULL,
-picture	TEXT,
+usr_id		TEXT,
+usr_name	TEXT,
+picture		TEXT,
 target_time	INTEGER,
 hello_time	INTEGER,
 rate		INTEGER,
-quize		INTEGER,
-PRIMARY KEY (usr_id)
+quize		INTEGER
 );
